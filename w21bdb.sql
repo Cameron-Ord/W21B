2322ba4c183fc09da91a15dc4768621ff84982bd
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-06-10 14:36:11 -0600

    fixed a proceedure not giving the correct results

--- w21bdb.sql
+++ w21bdb.sql
@@ -31,7 +31,7 @@ CREATE TABLE `philosopher` (
   `image_url` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `philosopher_UN` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `philosopher` (
 
 LOCK TABLES `philosopher` WRITE;
 /*!40000 ALTER TABLE `philosopher` DISABLE KEYS */;
-INSERT INTO `philosopher` VALUES (2,'Socrates','Socrates (c. 469 - 399 B.C.) was a hugely important Greek philosopher from the Classical period (often known as the Socratic period in his honor). Unlike most of the Pre-Socratic philosophers who came before him, who were much more interested in establishing how the world works, Socrates was more concerned with how people should behave, and so was perhaps the first major philosopher of Ethics.','469 BC','399 BC','https://www.philosophybasics.com/photos/socrates.jpg'),(3,'Thomas Hobbes','Thomas Hobbes (1588 - 1679) was an English philosopher of the Age of Reason. His famous 1651 book \'Leviathan\' and his social contract theory, developed during the tumultuous times around the English Civil War, established the foundation for most of Western Political Philosophy.','1588','1679','https://www.philosophybasics.com/photos/hobbes.jpg'),(5,'Martin Heidegger','Martin Heidegger (1889 - 1976) was a 20th Century German philosopher. He was one of the most original and important philosophers of the 20th Century, but also one of the most controversial. His best known book, Being and Time, although notoriously difficult, is generally considered to be one of the most important philosophical works of the 20th Century.','1889','1976','https://www.philosophybasics.com/photos/heidegger.jpg');
+INSERT INTO `philosopher` VALUES (2,'Socrates','Socrates (c. 469 - 399 B.C.) was a hugely important Greek philosopher from the Classical period (often known as the Socratic period in his honor). Unlike most of the Pre-Socratic philosophers who came before him, who were much more interested in establishing how the world works, Socrates was more concerned with how people should behave, and so was perhaps the first major philosopher of Ethics.','469 BC','399 BC','https://www.philosophybasics.com/photos/socrates.jpg'),(3,'Thomas Hobbes','Thomas Hobbes (1588 - 1679) was an English philosopher of the Age of Reason. His famous 1651 book \'Leviathan\' and his social contract theory, developed during the tumultuous times around the English Civil War, established the foundation for most of Western Political Philosophy.','1588','1679','https://www.philosophybasics.com/photos/hobbes.jpg'),(5,'Martin Heidegger','Martin Heidegger (1889 - 1976) was a 20th Century German philosopher. He was one of the most original and important philosophers of the 20th Century, but also one of the most controversial. His best known book, Being and Time, although notoriously difficult, is generally considered to be one of the most important philosophical works of the 20th Century.','1889','1976','https://www.philosophybasics.com/photos/heidegger.jpg'),(6,'Heraclitus of Ephesus','Heraclitus of Ephesus (c. 535 - 475 B.C.) was a Pre-Socratic Greek philosopher from Ephesus, on the Ionian coast of modern-day Turkey. He is sometimes mentioned in connection with the Ephesian School of philosophy, although he was really the only prominent member of that school (which, along with the Milesian School, is often considered part of the Ionian School','535 BC','475 BC','https://www.philosophybasics.com/photos/heraclitus.jpg');
 /*!40000 ALTER TABLE `philosopher` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +58,7 @@ CREATE TABLE `quote` (
   PRIMARY KEY (`id`),
   KEY `quote_FK` (`philosopher_id`),
   CONSTRAINT `quote_FK` FOREIGN KEY (`philosopher_id`) REFERENCES `philosopher` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `quote` (
 
 LOCK TABLES `quote` WRITE;
 /*!40000 ALTER TABLE `quote` DISABLE KEYS */;
-INSERT INTO `quote` VALUES (1,2,'The unexamined life is not worth living'),(2,2,'There is only one good, knowledge, and one evil, ignorance'),(3,2,'Ooga booga me shooty da bug');
+INSERT INTO `quote` VALUES (1,2,'The unexamined life is not worth living'),(2,2,'There is only one good, knowledge, and one evil, ignorance'),(3,2,'Ooga booga me shooty da bug'),(4,3,'Bingo bango bongo I don\'t want to leave the congo');
 /*!40000 ALTER TABLE `quote` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -152,7 +152,7 @@ DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `return_spec_p`(p_id_i int unsigned)
 BEGIN
 	select convert(name using "utf8") as name,`quote`.id,convert(content using "utf8") as content from `quote`
-	inner join philosopher where `quote`.philosopher_id=philosopher.id;
+	inner join philosopher where `quote`.philosopher_id=philosopher.id and `quote`.philosopher_id=p_id_i;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -169,4 +169,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-10 14:25:13
+-- Dump completed on 2023-06-10 14:35:47
